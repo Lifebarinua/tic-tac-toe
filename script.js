43e7fc7148c5916af67179c4dadf24944a5a8245
Lifebarinua <barinualife@gmail.com>
2025-08-25 18:35:07 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const Gameboard = {
+    board: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ],
+
+  // optional: method to print the board
+  printBoard() {
+    console.log(this.board[0]);
+    console.log(this.board[1]);
+    console.log(this.board[2]);
+  }
+};
+
